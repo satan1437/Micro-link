e6ed9961f76fc02524ef9c7c56cdd1db253422dd
Satan1437 <exemplles.white@gmail.com>
2022-08-15 17:03:05 +0300

    :chicken: fix long url

--- links/admin.py
+++ links/admin.py
@@ -5,7 +5,7 @@ from .models import Link
 
 @admin.register(Link)
 class LinkAdmin(admin.ModelAdmin):
-	list_display = ('owner', 'url', 'shorted_url', 'redirection', 'date')
+	list_display = ('owner', 'short_origin_url', 'shorted_url', 'redirection', 'date')
 	list_filter = ('date',)
 	search_fields = ('owner__username', 'url', 'date')
 	search_help_text = 'Поиск осуществляется по Никнейму, URL и Дате.'
@@ -20,4 +20,10 @@ class LinkAdmin(admin.ModelAdmin):
 			'fields': ('url', 'shorted_url')
 		})
 	)
-	readonly_fields = ('owner', 'redirection', 'shorted_url', 'date')
+	readonly_fields = ('redirection', 'shorted_url', 'date')
+
+	@admin.display(description='url')
+	def short_origin_url(self, obj):
+		if len(obj.url) >= 50:
+			return f'{obj.url[:50]}...'
+		return obj.url
